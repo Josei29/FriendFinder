505e8fb79c2b8a42504c8261753c60c586411a0d
Josei29 <jjimenez.r98@gmail.com>
2018-09-04 17:45:30 -0500

    First Update

---
+++ README.md
@@ -0,0 +1,29 @@
+# Friend Finder Application
+
+This is a basic version of a friend matching application.
+
+### Technologies used:
+* Node.js
+* Express
+* Javascript
+
+## Home Page
+This is the home page of the application and as you can see the URL is http://localhost:3000 wich means that the app is online running with the Express server we created. It has 3 buttons, Find Someone will take you to a new HTML document where you will be able to fill a form and answer some questions! With the Api Friends List you will be able to see the current list of friends the server has. 
+
+<img src="./screenshots/home.png"
+     alt="Screenshot"
+     style="margin-left: 40px; width:550px; height:350px" />
+
+## Survey
+In this part of the application you will be able to fill a form and answer some questions that later will create a new JSON Object and will be available for future matches, if the user goes to the  Friend Api link they will be able to see the recently added JSON Object.
+
+<img src="./screenshots/survey.png"
+     alt="Screenshot"
+     style="margin-left: 40px; width:550px; height:350px" />
+
+## Match
+After successfully filling and clicking the Done button, a dynamically generated modal will apear on the screen with the user match.
+
+<img src="./screenshots/match.png"
+     alt="Screenshot"
+     style="margin-left: 40px; width:550px; height:350px" />
---
+++ app/data/friends.js
@@ -0,0 +1,40 @@
+// Creating The Friends List
+var friends = [
+    {
+        name: "Dua Lipa",
+        age: 23,
+        link: "http://junkee.com/wp-content/uploads/2018/03/dua-bigger-head.png",
+        answers: ["2", "1", "1", "1", "3", "1", "2", "1", "4", "2"]
+    }, 
+    {
+        name: "Emma Stone",
+        age: 29,
+        link: "https://media.self.com/photos/585c0ea98a2a9595367fa2df/4:3/w_728,c_limit/emma-stone-anxiety.jpg",
+        answers: ["1", "2", "2", "2", "1", "3", "2", "2", "2", "3"]
+    },
+    {
+        name: "Sophie Turner",
+        age: 22,
+        link: "https://assets.vogue.com/photos/59482cc3704a2a32d66c6b75/master/pass/sophie-turner-got-lede.jpg",
+        answers: ["3", "3", "1", "1", "4", "4", "1", "1", "3", "1"]
+    },
+    {
+        name: "Conor McGregor",
+        age: 30,
+        link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqyFTvmciIu94MfbIzKUvTcTscniUrTKS7SKFsiBQILbyo_YcE",
+        answers: ["4", "4", "2", "1", "4", "4", "1", "3", "1", "2"]
+    },
+    {
+        name: "Post Malone",
+        age: 23,
+        link: "https://images.complex.com/complex/images/c_limit,w_680/fl_lossy,pg_1,q_auto/tkxndk4gl61ruxtjqtyb/post-malone-siriusxm",
+        answers: ["2", "1", "1", "1", "2", "4", "2", "2", "4", "2"]
+    },
+    {
+        name: "Lebron James",
+        age: 33,
+        link: "http://i.imgur.com/vP6fahI.jpg",
+        answers: ["3", "1", "1", "1", "3", "4", "2", "1", "4", "2"]
+    }];
+    
+module.exports = friends;
